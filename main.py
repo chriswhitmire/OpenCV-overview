af0cd135f868904222f43404980b572f05ba1934
chriswhitmire <chriswhitmire94@gmail.com>
2022-02-16 21:44:05 -0500

    Loaded and displayed an image

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ import cv2
 # Display an image
 ###########
 
+# load in the image and save it
+img = cv2.imread("Assets/amongUsDad.png")
+
+# display the image in its own window
+cv2.imshow("Among Us", img)
+
+# delay the window from closing automatically (0 as the parameter means that it will delay
+# forever until you press a key on the window or exit out of it)
+cv2.waitKey(0)
+
+# close the image window
+cv2.destroyAllWindows() 
