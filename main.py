62c7c206ac75695a8ae95b25c61579c9c0eaa2f7
chriswhitmire <chriswhitmire94@gmail.com>
2022-02-16 21:58:22 -0500

    Added code to load a video and show it

--- main.py
+++ main.py
@@ -4,15 +4,47 @@ import cv2
 # Display an image
 ###########
 
-# load in the image and save it
-img = cv2.imread("Assets/amongUsDad.png")
+# # load in the image and save it
+# img = cv2.imread("Assets/amongUsDad.png")
 
-# display the image in its own window
-cv2.imshow("Among Us", img)
+# # display the image in its own window
+# cv2.imshow("Among Us", img)
 
-# delay the window from closing automatically (0 as the parameter means that it will delay
-# forever until you press a key on the window or exit out of it)
-cv2.waitKey(0)
+# # delay the window from closing automatically (0 as the parameter means that it will delay
+# # forever until you press a key on the window or exit out of it)
+# cv2.waitKey(0)
 
-# close the image window
-cv2.destroyAllWindows() 
+# # close the image window
+# cv2.destroyAllWindows() 
+
+
+##########
+# Showing a video
+##########
+# load the video
+capture = cv2.VideoCapture("Assets/labAsSuicideSquad.mp4")
+
+# loop through every frame of the video and show it (stops when you reach the end of the video)
+while capture.isOpened():
+    # read the next frame of the video. "isLoaded" is a boolean representing whether the frame was loaded or not
+    isLoaded, img = capture.read()
+
+    if isLoaded == True:
+        # show the current frame in a window titled "Lab vid"
+        cv2.imshow("Lab vid", img)
+        # if you press the q key or click on the exit button, then close the video window (& is bitwise "and")
+        # This also makes it wait 25ms between each frame
+        if cv2.waitKey(25) & 0xFF == ord('q') or cv2.getWindowProperty("Lab vid", 0) < 0:
+            break
+
+    # if an image is not loaded, leave the loop
+    else:
+        break
+
+# When everything done, release the video capture object
+capture.release()
+   
+# Closes all the frames
+cv2.destroyAllWindows()
+
+
